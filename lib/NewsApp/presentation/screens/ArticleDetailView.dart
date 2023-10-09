9c5f75acf8a98b6ed0ff493de7eb163d6b7399f5
Bahaa-eddine-MB <bm.bouzeboudja@esi-sba.dz>
2023-10-09 14:51:01 +0200

    new page

---
+++ lib/NewsApp/presentation/screens/ArticleDetailView.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class ArticleDetailView extends StatelessWidget {
+  const ArticleDetailView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
